59457668160d6fbad0d18ab9945cdf1cac898a29
Dhanalakshmialaguraj <rjdhana41107@gmail.com>
2025-03-14 10:29:00 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,1 +1,31 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        
+        int lowerLimit = 1;
+        int upperLimit = 1000;
+
+        
+        for (int number = lowerLimit; number < upperLimit; number++) {
+            
+            int numOfDigits = String.valueOf(number).length();
+            int sum = 0;
+            int originalNumber = number;
+
+        
+            while (number != 0) {
+                int digit = number % 10;
+                sum += Math.pow(digit, numOfDigits);
+                number /= 10;
+            }
+
+            
+            if (sum == originalNumber) {
+                System.out.println(originalNumber + " is an Armstrong number.");
+            }
+
+            
+            number = originalNumber;
+        }
+    }
+}
 
